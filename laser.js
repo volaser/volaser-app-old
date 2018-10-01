d0e8f5fca0062e9d3c49d814a0bc347318ba5247
Matt Grau <matt.grau@gmail.com>
2018-10-01 02:31:25 +0200

    two way communication with laser via bluetooth

--- laser.js
+++ laser.js
@@ -10,6 +10,7 @@ export default class Laser extends React.Component {
     this.state = {
       info: "",
       values: {},
+      range: "",
       device: null
     };
     this.manager = new BleManager();
@@ -33,24 +34,27 @@ export default class Laser extends React.Component {
     this.setState({ info: "Error:" + message });
   }
 
-  async setupNotifications(device) {
-    const service = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
-    const characteristicW = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
-    const characteristic = await device.writeCharacteristicWithResponseForService(
-      service,
-      characteristicW,
-      Buffer.from("hello").toString("base64")
-    );
-  }
-
   sendBleMessage(message) {
-    const service = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
-    const characteristicW = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
-    this.state.device.writeCharacteristicWithoutResponseForService(
-      service,
-      characteristicW,
-      Buffer.from("hello").toString("base64")
-    );
+    this.state.device
+      .writeCharacteristicWithResponseForService(
+        "4fafc201-1fb5-459e-8fcc-c5c9c331914b",
+        "beb5483e-36e1-4688-b7f5-ea07361b26a8",
+        Buffer.from("hello").toString("base64")
+      )
+      .then(() => {
+        this.state.device
+          .readCharacteristicForService(
+            "4fafc201-1fb5-459e-8fcc-c5c9c331914b",
+            "a812aeed-78d0-474a-b9b1-20a8a1f95463"
+          )
+          .then(characteristic =>
+            this.setState({
+              range: Buffer.from(characteristic.value, "base64").toString(
+                "ascii"
+              )
+            })
+          );
+      });
   }
 
   scanAndConnect() {
@@ -59,25 +63,36 @@ export default class Laser extends React.Component {
       console.log(device.name);
       if (error) {
         this.error(error.message);
-        return;
-      }
-
-      if (device.name === "Volaser") {
-        this.info("Connecting to Volaser");
-        this.manager.stopDeviceScan();
-        device
-          .connect()
-          .then(device => {
-            this.info("Discovering services and characteristics");
-            return device.discoverAllServicesAndCharacteristics();
-          })
-          .then(
-            device => {
-              this.info("Listening...");
-              this.setState({ device: device });
-            },
-            error => this.error(error.message)
-          );
+      } else {
+        if (device.name === "Volaser") {
+          this.info("Connecting to Volaser");
+          this.manager.stopDeviceScan();
+          device
+            .connect()
+            .then(device => {
+              this.info("Discovering services and characteristics");
+              return device.discoverAllServicesAndCharacteristics();
+            })
+            .then(
+              device => {
+                this.info("Listening...");
+                this.setState({ device: device });
+                // device
+                //   .readCharacteristicForService(
+                //     "4fafc201-1fb5-459e-8fcc-c5c9c331914b",
+                //     "a812aeed-78d0-474a-b9b1-20a8a1f95463"
+                //   )
+                //   .then(characteristic =>
+                //     console.log(
+                //       Buffer.from(characteristic.value, "base64").toString(
+                //         "ascii"
+                //       )
+                //     )
+                //   );
+              },
+              error => this.error(error.message)
+            );
+        }
       }
     });
   }
@@ -85,6 +100,7 @@ export default class Laser extends React.Component {
   render() {
     return (
       <View>
+        <Text>Range: {this.state.range} m</Text>
         <Button
           large
           rounded
