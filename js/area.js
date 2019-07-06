e10293aafd026f4c2763a82408659a6d7bbca124
Matt Grau <matt.grau@gmail.com>
2019-07-06 20:52:38 +0200

    sort points in the display element

--- js/area.js
+++ js/area.js
@@ -4,11 +4,13 @@ import Svg, { Polygon, Polyline } from "react-native-svg";
 export default class Area extends React.Component {
   getAreaOutline() {
     if (this.props.points.length > 2) {
-      return this.props.points.reduce((outline, point) => {
-        let x = point["range"] * Math.cos((point["angle"] * Math.PI) / 180);
-        let y = point["range"] * Math.sin((point["angle"] * Math.PI) / 180);
-        return outline + ` ${x},${y}`;
-      }, "");
+      return this.props.points
+        .sort((a, b) => a.angle - b.angle)
+        .reduce((outline, point) => {
+          let x = point.range * Math.cos((point.angle * Math.PI) / 180);
+          let y = point.range * Math.sin((point.angle * Math.PI) / 180);
+          return outline + ` ${x},${y}`;
+        }, "");
     }
     return "0,0";
   }
