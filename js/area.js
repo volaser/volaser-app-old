b58d82b5d9cadfb5cfb7344b726ded3329c058ee
Matt Grau <matt.grau@gmail.com>
2019-07-07 19:26:30 +0200

    add grid lines to area

--- js/area.js
+++ js/area.js
@@ -1,5 +1,13 @@
 import React from "react";
-import Svg, { Rect, Polygon, Polyline } from "react-native-svg";
+import Svg, {
+  Polygon,
+  Path,
+  Defs,
+  Pattern,
+  Rect,
+  Polyline,
+  Line
+} from "react-native-svg";
 
 export default class Area extends React.Component {
   getAreaOutline() {
@@ -13,24 +21,43 @@ export default class Area extends React.Component {
     return "0,0";
   }
   render() {
-    const boxRadius = 3.9;
+    const Nlines = 20;
+    const xgrid = [...Array(Nlines).keys()].map((value, index) => (
+      <Line
+        key={"x" + index}
+        x1={value - Nlines / 2}
+        y1="-10"
+        x2={value - Nlines / 2}
+        y2="10"
+        stroke="gray"
+        strokeWidth="0.01"
+      />
+    ));
+    const ygrid = [...Array(Nlines).keys()].map((value, index) => (
+      <Line
+        key={"y" + index}
+        y1={value - Nlines / 2}
+        x1="-10"
+        y2={value - Nlines / 2}
+        x2="10"
+        stroke="gray"
+        strokeWidth="0.01"
+      />
+    ));
     return (
       <Svg
         height={this.props.height}
         width={this.props.width}
         viewBox={this.props.viewBox}
+        preserveAspectRatio="xMinYMin meet"
       >
-        <Rect x="-50%" y="-50%" width="100%" height="100%" fill="#eee" />
+        {/* <Rect x="-50%" y="-50%" width="100%" height="100%" fill="url(#grid)" /> */}
+        {xgrid}
+        {ygrid}
         <Polygon
           points={this.getAreaOutline()}
-          fill="#ccc"
+          fill="#175a91"
           stroke="black"
-          strokeWidth="0"
-        />
-        <Polyline
-          points={`${boxRadius},${boxRadius} -${boxRadius},${boxRadius} -${boxRadius},-${boxRadius}, ${boxRadius},-${boxRadius} ${boxRadius},${boxRadius}`}
-          fill="none"
-          stroke="gray"
           strokeWidth="0"
         />
       </Svg>
